8ebb4009daab30b2713837b994a5443a54fd3ce2
Jonas <jonasceolin@gmail.com>
2019-07-18 21:30:12 -0300

    refactoring

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './modules/area-de=trabalho/login/login.component';
 import { EnviroumentComponent } from './modules/enviroument/enviroument.component';
-import { ListaAtividadesComponent } from './modules/lista-atividades/lista-atividades.component';
-import { AtividadeComponent } from './modules/atividade/atividade.component';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent},
     { path: 'app', component: EnviroumentComponent,
-            children: [ 
-                { path: 'lista-atividades', component: ListaAtividadesComponent},
-                { path: 'atividade', component:AtividadeComponent}
-            ]
+        
     },
     { path: '', component: LoginComponent},
 
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -12,18 +12,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent } from './modules/area-de=trabalho/login/login.component';
-import { ListaAtividadesComponent } from './modules/lista-atividades/lista-atividades.component';
-import { AtividadeItemListComponent } from './modules/atividade-item-list/atividade-item-list.component';
-import { AtividadeComponent } from './modules/atividade/atividade.component';
 import { EnviroumentModule } from './modules/enviroument/enviroument.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		LoginComponent,
-		ListaAtividadesComponent,
-		AtividadeItemListComponent,
-		AtividadeComponent
 	],
 	imports: [
 		BrowserModule,
