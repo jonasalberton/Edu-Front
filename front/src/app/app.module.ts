f0e4f7ef71498de48f316a302b775c3e8a5421c8
Jonas <jonasceolin@gmail.com>
2019-07-19 10:09:21 -0300

    imports

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { AcessoModule } from './modules/acesso/acesso.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CommonModule } from '@angular/common';
 import { EnviroumentModule } from './modules/enviroument/enviroument.module';
+import { AcessoModule } from './modules/acesso/acesso.module';
 
 @NgModule({
 	declarations: [
@@ -18,9 +18,9 @@ import { EnviroumentModule } from './modules/enviroument/enviroument.module';
 		BrowserAnimationsModule,
 		AppRoutingModule,
 		CommonModule,
-		EnviroumentModule,
 		HttpClientModule,
-		AcessoModule
+		AcessoModule,
+		EnviroumentModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
