0065042a19276d67ce767eb8cefadae76cdcb293
Jonas <jonasceolin@gmail.com>
2019-09-01 19:26:50 -0300

    impl configuracoes

--- front/src/app/modules/usuario/configuracoes/configuracoes.component.ts
+++ front/src/app/modules/usuario/configuracoes/configuracoes.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/shared/models/usuario.model';
+import { LocalStorageService } from 'src/app/shared/service/local-storage.service';
 
 @Component({
-  selector: 'app-configuracoes',
-  templateUrl: './configuracoes.component.html',
-  styleUrls: ['./configuracoes.component.css']
+	selector: 'app-configuracoes',
+	templateUrl: './configuracoes.component.html',
+	styleUrls: ['./configuracoes.component.css']
 })
 export class ConfiguracoesComponent implements OnInit {
+	usuario: Usuario =  new Usuario;
 
-  constructor() { }
+	constructor(private localStorageService: LocalStorageService) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.buscarUsuario();
+	}
 
+	buscarUsuario(): void {
+		this.usuario = this.localStorageService.getUsuario();
+	}
 }
--- front/src/app/modules/usuario/usuario.module.ts
+++ front/src/app/modules/usuario/usuario.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';0
+
 import { ConfiguracoesComponent } from './configuracoes/configuracoes.component';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule, MatFormFieldModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
-  declarations: [ConfiguracoesComponent],
-  imports: [
-    CommonModule
-  ]
+	declarations: [
+		ConfiguracoesComponent
+	],
+	imports: [
+		CommonModule,
+		MatCardModule,
+		MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+	]
 })
 export class UsuarioModule { }
