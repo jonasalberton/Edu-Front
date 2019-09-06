b4416286e91ede4b0446dc147b55cca7dc8ba837
Jonas <jonasceolin@gmail.com>
2019-09-06 17:45:23 -0300

    lista atividades wp

--- front/src/app/modules/atividade/lista-atividades/lista-atividades.component.ts
+++ front/src/app/modules/atividade/lista-atividades/lista-atividades.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Atividade } from 'src/app/shared/models/atividade.model';
 
 @Component({
-  selector: 'app-lista-atividades',
-  templateUrl: './lista-atividades.component.html',
-  styleUrls: ['./lista-atividades.component.css']
+	selector: 'app-lista-atividades',
+	templateUrl: './lista-atividades.component.html',
+	styleUrls: ['./lista-atividades.component.css']
 })
 export class ListaAtividadesComponent implements OnInit {
+	atividades: Array<Atividade> = [];
+	constructor() { }
 
-  constructor() { }
+	ngOnInit() {
+		this.buildAtividadesMock();
+	}
 
-  ngOnInit() {
-  }
+	buildAtividadesMock(): void {
+		let atividade =  new Atividade();
+		atividade.avaliacao = 50;
+		atividade.descricao = 'ABCTSSDASDasdaasd';
+		atividade.nome = 'Javascript';
 
+
+		this.atividades.push(atividade);
+		this.atividades.push({...atividade});
+		this.atividades.push({...atividade});
+		this.atividades.push({...atividade});
+		this.atividades.push({...atividade, nome: 'Typescript'});
+		this.atividades.push({...atividade,  nome: 'Typescript'});
+		this.atividades.push({...atividade,  nome: 'Java'});
+		this.atividades.push({...atividade});
+		this.atividades.push({...atividade});
+		this.atividades.push({...atividade});
+	}
 }
